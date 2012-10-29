34c8b5f06b2f612a0f7059c42a809b447c8f95e7
znoraka@gmail.com <znoraka@gmail.com@f49f68ed-fd2e-9346-cfdb-b88dcf0d191b>
2012-10-29 02:20:45 +0000

    RemoveCross marche désormais correctement et permet bien d'enlever les croisements le bug venait du fait que double a = 1/3 est égal à 0, 1 et 3 sont considérés comme des int, il faut ajouter un cast

--- src/graphe/RemoveCross.java
+++ src/graphe/RemoveCross.java
@@ -10,46 +10,47 @@ public ArrayList<Integer> calculRMC(ArrayList<Integer> l, int [][] coordville, i
 	ArrayList<Integer> chemin = new ArrayList<Integer>(l);
 	int distemp;
 	int i = 1;
-	int j = 3;
-	
-while (i < chemin.size() - 3){
+	int j = 1;
+while (i < chemin.size() - 1){
 	while (j < chemin.size() - 1){
-		if (i == j){j = i+2;} //pour éviter de vérifier trois villes consecutives
+		//if (i == j){j = i+1;} //pour éviter de vérifier trois villes consecutives
 		if (detecteCroisement(chemin, coordville, i , j)){	//si un croisement est détecté on inverse les arètes
+			System.out.println("i : "+chemin.get(i) + " i+1 : "+chemin.get(i+1)+" j : "+chemin.get(j)+" j+1 : "+chemin.get(j+1));
 			distemp = chemin.get(i+1);
 			chemin.set(i+1, chemin.get(j));
 			chemin.set(j, distemp);
 			System.out.println(chemin);
 			i=1;
-			j=3;
+			j=1;
 			}
 		j++;
 		
 	}
 	i++;
-	j=3;
+	j=1;
 }
+
 return calculDistance(chemin, distville);	
 }	
 	
 
 public boolean detecteCroisement(ArrayList<Integer> l, int[][] coordville, int i, int j){
 	boolean bool = false;
-	System.out.println("droite entre : " + l.get(i) + " et " + l.get(i+1));
+	//System.out.println("droite entre : " + l.get(i) + " et " + l.get(i+1));
 	if(coordville[l.get(i+1)][1] != coordville[l.get(i)][1]){
 		//calcul du coefficient directeur de la premiere droite
-	double a1 = (coordville[l.get(i+1)][2]-coordville[l.get(i)][2]) / (coordville[l.get(i+1)][1]-coordville[l.get(i)][1]);
-	double b1 = coordville[l.get(i)][2] - (a1*coordville[l.get(i)][1]);
+	double a1 = (double)(coordville[l.get(i+1)][2]-coordville[l.get(i)][2]) / (double)(coordville[l.get(i+1)][1]-coordville[l.get(i)][1]);
+	double b1 = (double)(coordville[l.get(i)][2]) - (double)(a1*coordville[l.get(i)][1]);
 	
 	if (coordville[l.get(j+1)][1] != coordville[l.get(j)][1]){
 		//calcul du coefficient directeur de la deuxieme droite
-	double a2 = (coordville[l.get(j+1)][2]-coordville[l.get(j)][2]) / (coordville[l.get(j+1)][1]-coordville[l.get(j)][1]);
-	double b2 = coordville[l.get(j)][2] - (a2*coordville[l.get(j)][1]);
+	double a2 = (double)(coordville[l.get(j+1)][2]-coordville[l.get(j)][2]) / (double)(coordville[l.get(j+1)][1]-coordville[l.get(j)][1]);
+	double b2 = (double)(coordville[l.get(j+1)][2]) - (double)(a2*coordville[l.get(j+1)][1]);
 	
 	if (a1 != a2){
 		//calcul des coordonées du point d'intersection
-	int x = (int) ((b2 - b1) / (a1 - a2));
-	int y = (int) (a1*x + b1);
+	double x = (double)(b2 - b1) / (double)(a1 - a2);
+	double y = (a1*x + b1);
 	
 	if (x != 0 & appartientFigure(l, i,j, coordville, x, y )){ //on vérifie que le point d'intersection appartient bien aux segments et pas seulement aux droites
 		 
@@ -61,13 +62,13 @@ public boolean detecteCroisement(ArrayList<Integer> l, int[][] coordville, int i
 	return bool;
 }
 
-public boolean appartientFigure(ArrayList<Integer> l, int i, int j, int [][] coordville, int x,int y){
+public boolean appartientFigure(ArrayList<Integer> l, int i, int j, int [][] coordville, double x,double y){
 	boolean bool = false;
-	System.out.println("x : " + x + " y : " + y + " x1 : " + coordville[l.get(i)][1] + " x2 : " + coordville[l.get(i+1)][1] +" x3 : " + coordville[l.get(j)][1] +" x4 : " + coordville[l.get(j+1)][1]);
-	if(x < Math.max(coordville[l.get(i)][1], coordville[l.get(i+1)][1]) && x > Math.min(coordville[l.get(i)][1], coordville[l.get(i+1)][1])
-		&& x < Math.max(coordville[l.get(j)][1], coordville[l.get(j+1)][1]) && x > Math.min(coordville[l.get(j)][1], coordville[l.get(j+1)][1])){
-		if(y < Math.max(coordville[l.get(i)][2], coordville[l.get(i+1)][2]) && y > Math.min(coordville[l.get(i)][2], coordville[l.get(i+1)][2])
-				&& y < Math.max(coordville[l.get(j)][2], coordville[l.get(j+1)][2]) && y > Math.min(coordville[l.get(j)][2], coordville[l.get(j+1)][2])){	
+	//System.out.println("x : " + x + " y : " + y + " x1 : " + coordville[l.get(i)][1] + " x2 : " + coordville[l.get(i+1)][1] +" x3 : " + coordville[l.get(j)][1] +" x4 : " + coordville[l.get(j+1)][1]);
+	if(x+1 < Math.max(coordville[l.get(i)][1], coordville[l.get(i+1)][1]) && x-1 > Math.min(coordville[l.get(i)][1], coordville[l.get(i+1)][1])
+		&& x+1 < Math.max(coordville[l.get(j)][1], coordville[l.get(j+1)][1]) && x-1 > Math.min(coordville[l.get(j)][1], coordville[l.get(j+1)][1])){
+		if(y+1 < Math.max(coordville[l.get(i)][2], coordville[l.get(i+1)][2]) && y-1 > Math.min(coordville[l.get(i)][2], coordville[l.get(i+1)][2])
+				&& y+1 < Math.max(coordville[l.get(j)][2], coordville[l.get(j+1)][2]) && y-1 > Math.min(coordville[l.get(j)][2], coordville[l.get(j+1)][2])){	
 		bool = true;
 	}
 	}
