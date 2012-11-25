8d69c8b184491c90dfe5dccefb0e75c7feb28c3f
znoraka@gmail.com <znoraka@gmail.com@f49f68ed-fd2e-9346-cfdb-b88dcf0d191b>
2012-11-25 13:48:41 +0000

    modification du ppv pour qu'il démarre par toutes les villes et ne retourne que le meilleur trajet

--- src/graphe/PlusProcheVoisin.java
+++ src/graphe/PlusProcheVoisin.java
@@ -7,34 +7,47 @@ public class PlusProcheVoisin
 
 	
 	
-public ArrayList<Integer> calculPlusProcheVoisin(int v, ArrayList<Integer> liste, int [][] ville){
-	int villedep = v; //ville de départ
-	int ind = 0;	//indice de la case de l'arraylist (la ville) qui a la distance la plus courte à la ville précédente
-	int max = liste.size(); 
-	int [] distmin = {999999, v}; //distance bidon pour commencer
-	ArrayList<Integer> l = new ArrayList<Integer>(liste);
+public ArrayList<Integer> calculPlusProcheVoisin(int v, ArrayList<Integer> liste, int [][] distville){
+	Integer indk = v;
+	int villedep;
+	int distmin = distville[0][0];//distance minimum après parcours des villes
 	ArrayList<Integer> parcours = new ArrayList<Integer>();
-	parcours.add(0); //on initialise la liste dans laquelle sera stockée le parcours, la première case à 0, il y aura la distance
-	parcours.add(v); //dans la deuxième case, il y a la ville de départ, donnée en paramètre
-	
-		for(int i = 0; i < max; i++){
-			for(int j = 0; j < l.size(); j++){
-				if(ville[villedep][l.get(j)] < distmin[0]){ //on recherche la plus petite distance parmi toutes les distance disponibles à partir de 
-					distmin[0] = ville[villedep][l.get(j)];
-					distmin[1] = l.get(j);
-					ind = j;
-					}
-				}	
-		l.remove(ind);	//on enlève la ville à l'indice ind de la liste contenant les villes
-		parcours.set(0, distmin[0] + parcours.get(0)); //on met à jour le parcours
-		parcours.add(distmin[1]);
-		distmin[0] = 999999;
-		villedep = distmin[1];
+	ArrayList<Integer> bestparcours = new ArrayList<Integer>();
+	bestparcours.add(0);
+	bestparcours.set(0, 9999999);
+	parcours.add(0);
+	parcours.add(v);
+	for(int i = 0; i < distville.length; i++){
+		villedep = i;
+		ArrayList<Integer> l = new ArrayList<Integer>(liste);
+		parcours = new ArrayList<Integer>();
+		parcours.add(0);
+		parcours.add(villedep);
+		for(int j = 0; j < liste.size(); j++){
+			//on parcourt toutes les villes vérifiées pour trouver la distance ville vérifée-ville non vérifée la plus courte
+			for(int k = 0; k < l.size(); k++){
+				//on parcourt toutes les villes non vérifiées
+				if(distville[villedep][l.get(k)] < distmin){
+					//si la distance entre les deux villes plus courte
+					distmin = distville[villedep][l.get(k)];
+					indk = l.get(k);
+				}
+			}
+			parcours.add(indk);
+			l.remove(indk);
+			villedep = indk;
+			parcours.set(0, parcours.get(0) + distmin);
+			distmin = distville[0][0];
 	}
-	//on ajoute la première ville à la fin pour revenir point de départ
-	parcours.set(0, ville[parcours.get(1)] [parcours.get(parcours.size()-1)] + parcours.get(0));
-	parcours.add(parcours.get(1));
-	return parcours;
+		//on ajoute la première ville à la fin pour revenir point de départ
+		parcours.set(0, distville[parcours.get(1)] [parcours.get(parcours.size()-1)] + parcours.get(0));
+		System.out.println(parcours.get(0));
+		if(parcours.get(0) < bestparcours.get(0))
+			bestparcours = new ArrayList<Integer>(parcours);
+}	
+	
+	bestparcours.add(bestparcours.get(1));
+	return bestparcours;
 }
 
 
